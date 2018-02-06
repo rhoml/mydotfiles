52b05b931c1aff5bfdf38ef5da9ebd3b65167855
Rhommel Lamas <roml@rhommell.com>
2018-02-06 16:03:52 +1100

    Updates OSX configs

--- .zsh.d/osx/set-defaults.sh
+++ .zsh.d/osx/set-defaults.sh
@@ -6,36 +6,92 @@
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 #
 # Run ./set-defaults.sh and you'll be good to go.
+set -e
 
-# Disable press-and-hold for keys in favor of key repeat.
+# Disable App Quarantine feature so you can run apps you download
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Use AirDrop over every interface. srsly this should be a default.
+# Use AirDrop over every interface. srsly this should be a default
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Always open everything in Finder's list view. This is important.
+# Always open everything in Finder's list view. This is important
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder.
+# Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Set a really fast key repeat
+defaults write -g KeyRepeat -int 2
+defaults write -g InitialKeyRepeat -int 15
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+# Disable autocorrect
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable tap to click"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write -g com.apple.mouse.tapBehavior -int 1
+defaults write -g com.apple.mouse.tapBehavior -int 1
+
+# Show drives All
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
+# Empty trash securely
+defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+# Show warning before empty trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool true
+
+# Show warning before changing extensions
+defaults write com.apple.finder WarnOnEmptyTrash -bool true
+
+# Show filenames extensions
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Enable Autohide dock
+defaults write com.apple.dock autohide -bool true
+
+# Run the screensaver if we're in the bottom-left hot corner
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# Hide Safari's bookmark bar.
+# Ensure Dock icon size
+defaults write com.apple.dock tilesize -int 36
+
+# Sets dock icon magnification
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock largesize -int 80
+
+# Sets the dock position
+defaults write com.apple.dock orientation -string right
+
+# Define action for hot corner
+defaults write com.apple.dock wvous-tl-corner -int 4
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+defaults write com.apple.dock wvous-tr-corner -int 2
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Hide Safari's bookmark bar
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-# Set up Safari for development.
+# Set up Safari for development
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write -g WebKitDeveloperExtras -bool true
+
+# Disable creation of .DS_Store files on network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+/usr/bin/killall Dock
