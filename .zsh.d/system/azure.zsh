4d4b7c53168ae0d71973125177ff9344eaee4cd8
Rhommel Lamas <roml@rhommell.com>
2018-10-16 12:17:26 +1100

    Adds keychain reset password alias and ensure aws-azure-login uses gui mode

--- .zsh.d/system/azure.zsh
+++ .zsh.d/system/azure.zsh
@@ -1,2 +1,2 @@
 # All aliases that allows us to log into AWS
-alias aws-azure-login='docker run --rm -it -v ~/.aws:/root/.aws dtjohnson/aws-azure-login'
+alias aws-azure-login='/usr/local/bin/aws-azure-login --mode gui'
---
+++ .zsh.d/system/keychain-password-reset.zsh
@@ -0,0 +1,1 @@
+alias keychain-password-reset='security set-keychain-password /Users/rlamas/Library/Keychains/login.keychain-db'
